a1767191b970fbb9336ff5c67f35f7a7ca8e06ce
shajib126 <shajib126@gmail.comm>
2020-09-23 21:38:21 +0600

    todays_task_done

---
+++ jubaer/continue.js
@@ -0,0 +1,7 @@
+for(let i = 0; i<=10; i++){
+    if(i ===3 || i === 7){ //if it's 1 or 7 then dont read this just continue or skip
+        continue
+    }else{
+        console.log(i)
+    }
+}
---
+++ jubaer/string.js
@@ -0,0 +1,12 @@
+// //in javascript have two type of string
+// // 1. string literal 2. string constructor
+
+var str = "strign" //this is string literal
+var strC = String('String ') //this is string constructor
+
+
+var n = 10;
+var str = String(n)
+console.log(str)
+
+

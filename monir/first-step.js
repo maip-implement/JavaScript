1a5b1abc0208b213879f57ebcd7e19deb652be5c
moniruddin <monirkm74@gmail.com>
2020-09-23 22:38:17 +0600

    How to Print Hello World in Js

--- monir/first-step.js
+++ monir/first-step.js
@@ -1,1 +1,10 @@
-console.log("hlw");
+//Called String
+console.log("This is a sting");
+
+
+// also Called String because of quotation
+console.log("999");
+
+// also Called String because of quotation
+console.log("node" + 9);
+

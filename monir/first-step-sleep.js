39b5f9001408c5e166f29160595089c625dded8e
moniruddin <monirkm74@gmail.com>
2020-09-24 01:35:42 +0600

    Base

---
+++ monir/first-step-sleep.js
@@ -0,0 +1,39 @@
+//Called String
+console.log("This is a sting");
+// also Called String because of quotation
+console.log("999");
+// Called String
+console.log("node" + 9);
+
+
+// Called Variable
+ var name = "Monir Uddin";
+ console.log("this is " + name + ".");
+
+
+//date type
+
+// primitive Date type(5)
+// 1.Number 10, 12, 99.2
+// 2.Boolean true False
+//3.null
+// 4.undifined
+// 5.String
+
+// Object
+//array
+// object
+//function
+
+
+var a = 11;
+var b = 12;
+
+
+console.log(a++);  //result = 12
+console.log(++a); //result = 11
+console.log(a); // result= 12
+
+
+
+

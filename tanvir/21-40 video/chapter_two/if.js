c69d3a3d61dd397625871c67b95fb9095f2bdab0
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-09-23 21:25:43 +0600

    2nd day

---
+++ tanvir/21-40 video/chapter_two/if.js
@@ -0,0 +1,22 @@
+//021. Logic and Condition in programming
+
+/*
+if
+else
+else if
+switch
+*/
+
+//022. What is If Conditions in Javascript
+
+var a = 10
+var b = 20
+
+//a>b true or false
+
+if(a>b){
+    console.log('A is greater than B')
+}
+if(b>a){
+    console.log('B is greater than A')
+}

01b64478b65d1b8c21615e3f2828ae66da003dc3
tts-doveloper2 <ttsjk.2020.02@gmail.com>
2020-09-25 01:57:00 +0800

    custom string length

--- arif/4. Handling Strings/string-length.js
+++ arif/4. Handling Strings/string-length.js
@@ -1,7 +1,19 @@
 // sting length by custom function
-
 const len = str => {
+    let count = 0
+
     while (true) {
-        if
+        if (str.charAt(count) === '') break
+        count++
     }
+
+    return count
 }
+
+console.log(len('abcde'))  // 5
+console.log(len('Md Arif Hossain'))  // 15
+
+
+// default method to get length of a string
+console.log('abcde'.length)  // 5
+console.log(len('Md Arif Hossain'))  // 15

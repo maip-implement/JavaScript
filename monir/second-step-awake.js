078d8fbf71c40518a7cf141050127ec2ec8b7f4c
moniruddin <monirkm74@gmail.com>
2020-09-24 23:50:14 +0600

    base

---
+++ monir/second-step-awake.js
@@ -0,0 +1,43 @@
+//Condition
+
+// var a = 100;
+// var b = 20;
+
+// if(a>b){
+//     console.log("a is greater then b");
+// }
+// if(a<b){
+//     console.log("a is greater then b");
+// }
+
+// if(a>b){
+//     console.log("a is greater then b");
+// }else{
+//     console.log("b is greater then a");
+// }
+
+
+// var n = 5;
+// if(n%2 == 0){
+//     console.log("n is Event Number");
+// }else{
+//     console.log("n is odd Number");
+// }
+
+// var a = 20;
+// var b = 2;
+
+// if(a>b){
+//     console.log("a is greater then b");
+// }else if(a<b ){ 
+//     console.log("b is greater then a");
+// }else{
+//     console.log("Both are same");
+// }
+
+
+//switce statement
+
+var date = new Date();
+var today = date.getDAy()
+console.log(today);

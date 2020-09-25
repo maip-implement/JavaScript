faf30660bdc62d86cc969e389678397d44e0518c
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-09-26 00:23:31 +0600

    map details explained with implementation

--- sakil/map.js
+++ sakil/map.js
@@ -9,16 +9,26 @@ console.log(sqrArr)
 
 
 // implementation
-function map(arr){
+function map(arr, cbFunc){
     var newArr = []
     for(i = 0; i < arr.length; i++){
-        var temp = arr[i] * arr[i]
+        var temp = cbFunc(arr[i], i, arr)
         newArr.push(temp)
     }
     return newArr
 }
 
-console.log(map(arr))
+var qb = map(arr, function(value){
+    return value * value * value;
+})
+
+var mTen = map(arr, function(value){
+    return value * 10
+})
+
+console.log(arr)
+console.log(qb, mTen)
+
 
 
 //Difference between forEach and map method is forEach will change the original array and map will create a different array from the original one.

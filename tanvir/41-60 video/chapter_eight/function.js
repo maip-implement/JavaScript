b3db13d577b5a29b841d102c25e0c4e4e096a1f5
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-09-28 10:02:55 +0600

    function invoking

--- tanvir/41-60 video/chapter_eight/function.js
+++ tanvir/41-60 video/chapter_eight/function.js
@@ -4,20 +4,60 @@
 
 // ***** 060 Defining a Function in Javascript | Stack Learner *****
 
-function functionName() {
+// function functionName() {
     
-}
+// }
 
-function sum() {
-    var a = 10
-    var b = 20
-    console.log(a+b)
+// function sum() {
+//     var a = 10
+//     var b = 20
+//     console.log(a+b)
     
-}
+// }
 
-function sub() {
-    var a = 10
-    var b = 20
-    console.log(a-b)
+// function sub() {
+//     var a = 10
+//     var b = 20
+//     console.log(a-b)
     
-}
+// }
+
+// ***** 061 Invoking a Function in Javascript | Stack Learner *****
+
+// function functionName() {
+//     console.log('I am a Function')
+//     }
+    
+//     function sum() {
+//         var a = 10
+//         var b = 20
+//         console.log(a+b)
+        
+//     }
+    
+//     function sub() {
+//         var a = 10
+//         var b = 20
+//         console.log(a-b)
+        
+//     }
+
+//     functionName()
+
+//     for(var i = 0; i<5; i++){
+//         functionName()
+//     }
+
+//     sum()
+//     sub()
+
+// I am a Function
+
+// I am a Function
+// I am a Function
+// I am a Function
+// I am a Function
+// I am a Function
+
+// 30
+// -10

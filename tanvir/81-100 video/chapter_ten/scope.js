6f0ce38ba1846a5042559e9097081ed6855c7395
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-09-30 12:25:42 +0600

    scope

--- tanvir/81-100 video/chapter_ten/scope.js
+++ tanvir/81-100 video/chapter_ten/scope.js
@@ -32,3 +32,36 @@
 }
 
 // 55
+
+//094 Lexical Scope in Javascript | Stack Learner
+
+// 095 Scope Chain in Javascript | Stack Learner
+
+var a  = 11
+
+function A(){
+    var b = 12
+    function B(){
+        var c = 23
+        console.log(c)
+    }
+    function C(){
+        var d = 56
+        console.log(d)
+    }
+    console.log(b)
+
+    D()
+    B()
+    C()
+
+}
+
+function D(n){
+    return n + a
+}
+
+// A -> a, b, B(), C(), D()
+// B -> a, b, c, B(), C(), D()
+// C -> a, b, d, B(), C(), D()
+// D -> a, n, A()

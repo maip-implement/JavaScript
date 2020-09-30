4972333fc124c7bd869aee22150d42e05d1e2fab
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-09-30 13:00:43 +0600

    closure and loops

--- tanvir/81-100 video/chapter_ten/closer.js
+++ tanvir/81-100 video/chapter_ten/closer.js
@@ -14,15 +14,43 @@
 
 // I am Lerning Lexical Scope  and Closure
 
-function test(){
-    var msg = 'I am Lerning Lexical Scope  and Closure'
+// function test(){
+//     var msg = 'I am Lerning Lexical Scope  and Closure'
 
-   return function(){
-        console.log(msg)
-    }
-}
+//    return function(){
+//         console.log(msg)
+//     }
+// }
 
-var sayMsg = test()
-sayMsg()
+// var sayMsg = test()
+// sayMsg()
 
 //I am Lerning Lexical Scope  and Closure
+
+// ***** 097 Closure and Loops in Javascript | Stack Learner
+
+// for(var i = 1; i <= 5; i++){
+//     setTimeout(function(){
+//         console.log(i)
+//     }, 1000*i)
+// }
+
+// 6
+// 6
+// 6
+// 6
+// 6
+
+for(var i = 1; i <= 5; i++){
+  (function(n){
+      setTimeout(function(){
+          console.log(n)
+      }, 1000 * n) 
+    })(i)    
+}
+
+// 1
+// 2
+// 3
+// 4
+// 5

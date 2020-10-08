db4f530618401ad3af6990f62a3d39bbbb2aeefc
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-10-08 21:23:03 +0600

    get set

--- tanvir/101-115 video/chapter_twelve/index.js
+++ tanvir/101-115 video/chapter_twelve/index.js
@@ -303,27 +303,77 @@
 
 //120 Getter Setter in Javascript | Stack Learner
 
+// var Rectangle = function(width, height) {
+
+//     var position = {
+//         x: 56,
+//         y: -100
+//     }
+//     this.width = width
+//     this.height = height
+    
+//            var printProperties = function() {
+//             console.log('My Width is ' + this.width)
+//             console.log('My Height is ' + this.height)
+//         }.bind(this)
+        
+//         this.getPosition = function() {
+//             return position
+//         }
+
+//         this.draw = function() {
+//             console.log('I am a rectangle')
+//             printProperties()
+//             console.log('Position: X=' + position.x + ' Y=' + position.y)
+                        
+//          }
+//     }
+    
+//      var rect7 = new Rectangle(45, 30)
+//     rect7.draw()
+//     console.log(rect7.getPosition)
+
+// I am a rectangle
+// My Width is 45
+// My Height is 30
+// Position: X=56 Y=-100
+
 var Rectangle = function(width, height) {
 
-    var position = {
-        x: 56,
-        y: -100
-    }
-    this.width = width
-    this.height = height
-    
-           var printProperties = function() {
-            console.log('My Width is ' + this.width)
-            console.log('My Height is ' + this.height)
-        }.bind(this)
-    
-        this.draw = function() {
-            console.log('I am a rectangle')
-            printProperties()
-            console.log('Position: X=' + position.x + ' Y=' + position.y)
-                        
+var position = {
+    x: 56,
+    y: -100
+}
+
+this.width = width
+this.height = height
+
+       var printProperties = function() {
+        console.log('My Width is ' + this.width)
+        console.log('My Height is ' + this.height)
+    }.bind(this)
+
+    this.draw = function() {
+        console.log('I am a rectangle')
+        printProperties()
+        console.log('Position: X=' + position.x + ' Y=' + position.y)
+                    
+     }
+
+     Object.defineProperty(this, 'position',{
+         get: function(){
+            return position 
+         },
+         set: function(value) {
+             position = value
          }
-    }
-    
-     var rect7 = new Rectangle(45, 30)
-    rect7.draw()
+     })
+}
+
+ var rect7 = new Rectangle(45, 30)
+rect7.draw()
+rect7.position = {
+    x: 456,
+    y: -123
+}
+console.log(rect7.position)

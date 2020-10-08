5b7e277d69822c2c43ac0d54111ef8a492a2a95c
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-10-08 20:54:35 +0600

    passbyvalue passbyreference

--- tanvir/101-115 video/chapter_twelve/index.js
+++ tanvir/101-115 video/chapter_twelve/index.js
@@ -242,3 +242,88 @@
 
 // 117 Pass By Value vs Pass by Reference in Javascript | Stack Learner
 
+// var n = 10
+
+// function change(n) {
+//     n = n + 100
+//     console.log(n)
+// }
+
+// change(n)   //110
+// console.log(n)  //10
+
+// var obj = {
+//     a: 10,
+//     b: 20
+// }
+
+// function changeMe(obj) {
+//     obj.a = obj.a + 100
+//     obj.b = obj.b + 100
+//     console.log(obj)
+// }
+
+// changeMe(obj)   //{a: 110, b: 120}
+// console.log(obj)    //{a: 110, b: 120}
+
+//118 Abstraction in Javascript | Stack Learner
+
+// 119 Private Properties in Javascript | Stack Learner
+
+
+// var Rectangle = function(width, height) {
+
+// var position = {
+//     x: 56,
+//     y: -100
+// }
+// this.width = width
+// this.height = height
+
+//        var printProperties = function() {
+//         console.log('My Width is ' + this.width)
+//         console.log('My Height is ' + this.height)
+//     }.bind(this)
+
+//     this.draw = function() {
+//         console.log('I am a rectangle')
+//         printProperties()
+//         console.log('Position: X=' + position.x + ' Y=' + position.y)
+                    
+//      }
+// }
+
+//  var rect7 = new Rectangle(45, 30)
+// rect7.draw()
+
+// I am a rectangle
+// My Width is 45
+// My Height is 30
+// Position: X=56 Y=-100
+
+//120 Getter Setter in Javascript | Stack Learner
+
+var Rectangle = function(width, height) {
+
+    var position = {
+        x: 56,
+        y: -100
+    }
+    this.width = width
+    this.height = height
+    
+           var printProperties = function() {
+            console.log('My Width is ' + this.width)
+            console.log('My Height is ' + this.height)
+        }.bind(this)
+    
+        this.draw = function() {
+            console.log('I am a rectangle')
+            printProperties()
+            console.log('Position: X=' + position.x + ' Y=' + position.y)
+                        
+         }
+    }
+    
+     var rect7 = new Rectangle(45, 30)
+    rect7.draw()

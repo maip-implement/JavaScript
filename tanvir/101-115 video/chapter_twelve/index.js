062cf9ad16841dd164cd6c1d27952dbbd937779e
Tanvir Hossain Fahim <45764618+TanvirFahimBD@users.noreply.github.com>
2020-10-08 18:31:21 +0600

    function object

--- tanvir/101-115 video/chapter_twelve/index.js
+++ tanvir/101-115 video/chapter_twelve/index.js
@@ -181,5 +181,37 @@
 
 // 114 Constructor Property in Javascript | Stack Learner
 
-var str = new String('str')
-console.log('My String is ' + str)
+// var str = new String('str')
+// console.log('My String is ' + str)
+
+// My String is str
+
+
+//115 Functions Are Object in Javascript | Stack Learner
+
+function test() {
+    console.log('Something')
+}
+
+console.log(test.name, test.length)
+
+//test 0
+
+var Rect = new Function('width', 'height', `this.width = width
+this.height = height
+        
+this.draw = function() {
+    console.log('I am a rectangle')
+    this.printProperties()
+    console.log(this)
+                
+            }
+        
+            this.printProperties = function(){
+                console.log('My Width is ' + this.width)
+                console.log('My Height is ' + this.height)
+    }`
+ )
+
+ var rect5 = new Rect(4, 5)
+ console.log(rect5)
